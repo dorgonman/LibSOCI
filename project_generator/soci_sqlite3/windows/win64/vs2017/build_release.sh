b8352ae96d35712687a6682dfe998954a49fd79a
dorgon.chang <dorgonman@hotmail.com>
2017-12-24 16:36:33 +0800

    add vs 2017 build

---
+++ project_generator/soci_sqlite3/windows/win64/vs2017/build_release.sh
@@ -0,0 +1,13 @@
+set -e
+
+CONFIG_PATH=$(pwd)/release.xml
+echo CONFIG_PATH:${CONFIG_PATH}
+pushd ../../../../../../../python/HorizonBuildTool/HorizonBuildTool/
+
+python Source/HorizonCMakeBuild/Main.py --clean --config=${CONFIG_PATH}
+python Source/HorizonCMakeBuild/Main.py --config=${CONFIG_PATH}
+popd
+
+
+cp -r ../../../../../intermediate/project/win64/vs2015/Release/lib/Release/ \
+	  ../../../../../libs/win64/vs2015
